841f0adda114bd63a94cd07dfb63789af53049e3
og8217 <og8217@intl.att.com>
2023-03-04 23:08:46 +0200

    remove old code

--- packages/package-04/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-
-declare module 'wc-jsx' {
-    export const WC: {
-        createElement: (tag, props, ...children) => HTMLElement;
-        createFragment: (props, ...children) => HTMLElement;
-    }
-    export const createElement: (tag, props, ...children) => HTMLElement;
-    export const createFragment: (props, ...children) => HTMLElement;
-}
