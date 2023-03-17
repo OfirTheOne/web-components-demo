29ff6686cb51adc331ec5d51c22b2cb038c58174
og8217 <og8217@intl.att.com>
2023-03-17 21:49:54 +0200

    add componentInstance: IPresentable to PreserveElementState

--- single-client/src/lib/core/renderer/wc-renderer/types.ts
+++ single-client/src/lib/core/renderer/wc-renderer/types.ts
@@ -1,8 +1,10 @@
+import { IPresentable } from "src/lib/models";
 import { VirtualElement } from "../../../models/virtual-element";
 
 export interface PreserveElementState<E extends HTMLElement = HTMLElement> {
     state: Record<any, any>;
     preserved?: E;
+    componentInstance: IPresentable
 }
 
 export type PreserveElementStateMap = Map<string, PreserveElementState>;  
