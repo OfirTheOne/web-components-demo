abb19162e7a6a7bfb50f12ce2b485a7866eec2bb
og8217 <og8217@intl.att.com>
2023-04-08 00:55:30 +0300

    WIP

--- single-client/src.functional/app/app.tsx
+++ single-client/src.functional/app/app.tsx
@@ -1,62 +1,12 @@
 
 import { WC } from "../lib/jsx";
-import { createRef, createSignal } from "../lib/core";
-import { useEffect } from "../lib/core/hooks/use-effect";
+import { Game } from "./tic-tac-toe";
 
-function SearchBar()  {
-    const [getText, setText] = createSignal('');
-
-    useEffect(() => {
-        console.log('SearchBar useEffect');
-        if(getText().length > 3) {
-            alert(getText());
-        }
-    }, [getText()]);
-
-    return <div>
-        <Input
-            text={getText()}
-            onKeyup={(e) => setText(e.target.value)}
-        />
-        <Button text="Search" onClick={() => alert(getText())} />
-    </div>;
-}
 
 export function App() {
     return (<div>
-        <h1>Single Client</h1> 
-        <SearchBar />   
+        <h1>TicTacToe Example</h1> 
+        <Game />
     </div>);
 }
    
-interface ButtonProps {
-    onClick: (e: any) => void;
-    text: string;
-}
-
-function Button(props: ButtonProps) {
-    const [getButton, setButton] = createRef<HTMLButtonElement>();
-    console.log(getButton() instanceof HTMLButtonElement);
-    
-    return <button
-        ref={setButton}
-        onClick={props.onClick}
-    >{props.text}</button>;
-}
-
-
-interface InputProps {
-    onKeyup: (e: any) => void;
-    text: string;
-}
-
-function Input(props: InputProps) {
-    console.log('Input text', props.text);
-    return <input 
-        value={props.text}
-        onKeyup={props.onKeyup}
-    />;
-}
-
-
- 
