6829e2abc6693c6854395bcc86db8a96987a8d97
og8217 <og8217@intl.att.com>
2023-04-25 19:44:45 +0300

    clean

--- single-client/src.functional/lib/models/index.ts
+++ single-client/src.functional/lib/models/index.ts
@@ -1,9 +1,6 @@
 export * from './container-meta';
 export * from './ctor';
-export * from './hooks';
 export * from './props';
 export * from './virtual-element';
 export * from './dom-element';
-// export * from './wc-container-options';
-// export * from './i-presentable';
-// export * from './define-component-options';
+
