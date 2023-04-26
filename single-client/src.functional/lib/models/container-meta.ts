d8b5e6302cb8ac9ac4b83d7ce1808d34830a075b
og8217 <og8217@intl.att.com>
2023-04-26 17:10:37 +0300

    clean

--- single-client/src.functional/lib/models/container-meta.ts
+++
@@ -1,3 +0,0 @@
-export interface PresentableMeta {
-  presentableName: string;
-}
--- single-client/src.functional/lib/models/index.ts
+++ single-client/src.functional/lib/models/index.ts
@@ -1,5 +1,4 @@
-export * from './container-meta';
-export * from './ctor';
+export * from '../types/ctor';
 export * from './props';
 export * from './virtual-element';
 export * from './dom-element';
