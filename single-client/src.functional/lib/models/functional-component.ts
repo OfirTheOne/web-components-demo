5a0f7b1ebf047c10bc5dd7f9a7c8e761c9c6674c
og8217 <og8217@intl.att.com>
2023-05-03 01:08:38 +0300

    add FunctionalComponent type definition

---
+++ single-client/src.functional/lib/models/functional-component.ts
@@ -0,0 +1,9 @@
+import { Props } from "./props";
+
+
+export interface FunctionalComponent<P extends object = object> {
+    (props: P, children: JSX.Element[]): JSX.Element;
+
+}
+
+export type FC<P extends object = object> = FunctionalComponent<P>;
