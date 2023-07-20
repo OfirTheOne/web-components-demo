586d776c3700d283bdfad5f35bfb3539073d58c9
og8217 <og8217@intl.att.com>
2023-07-21 02:30:48 +0300

    add exports

--- src/lib/core/signal-core/index.ts
+++ src/lib/core/signal-core/index.ts
@@ -1,5 +1,7 @@
 export * from './control-flow';
 export * from './signal/create-signal';
+export * from './signal/create-derived-signal';
+export * from './signal/create-resource';
 export * from './hooks/create-effect';
 export * from './signal-component/signal-component';
 export * from './models';
