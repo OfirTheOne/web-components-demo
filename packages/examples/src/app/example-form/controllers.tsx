f598f002df40540d1215792bf412ce65ce32d5ac
og8217 <og8217@intl.att.com>
2023-09-12 23:03:15 +0300

    wip

--- packages/examples/src/app/example-form/controllers.tsx
+++ packages/examples/src/app/example-form/controllers.tsx
@@ -1,38 +1,104 @@
-import Sig from "sig";
+import * as Sig from "sig";
 
 export const Input: Sig.FC<
-    { value: Sig.Signal<string> } & 
+    { value: Sig.Signal<string> } &
     Omit<JSX.HTMLAttributes<HTMLInputElement>, 'value'>
-> = ({value, ...props}) => {  
+> = ({ value, ...props }) => {
     return (
-        <input 
+        <input
             value={(value) as unknown as string}
             list={props.list}
-            onInput={(e) => value.setValue(() => e.target.value) }
+            onInput={(e) => value.setValue(() => e.target.value)}
             {...props} />
     );
 }
 
-export const Dropdown: Sig.FC<{ 
-    value: Sig.Signal<string>, 
+
+export const InputController: Sig.FC<{
+    value: Sig.Signal<string>,
+    label?: string,
+    id: string,
+    validation?: (value: string) => boolean,
+    validationError?: string
+} & Omit<JSX.HTMLAttributes<HTMLInputElement>, 'value' | 'id'>> = ({
+    value,
+    id,
+    label,
+    required = false,
+    validation,
+    validationError = '',
+    type = 'text',
+    ...props }) => {
+        const ref: { current: HTMLInputElement | null } = { current: null };
+        return (
+            <div>
+                <label htmlFor={id}>{label}</label>
+                <Input
+                    ref={ref}
+                    required={required}
+                    type={type}
+                    id={id}
+                    value={value}
+                    onInput={(e) =>{
+                        value.setValue(() => (e.target as HTMLInputElement).value)
+                    }}
+                    {...props}
+                />
+                {
+                    validation ?
+                        <Sig.Show
+                            track={value}
+                            when={([inputValue]) => {
+                                return (!ref.current || ref.current.checkValidity()) &&!validation(inputValue);
+                            }}
+                        >
+                            <label className="error">{validationError}</label>
+                        </Sig.Show>
+                        : null
+                }
+            </div>
+        );
+    }
+
+
+export const Dropdown: Sig.FC<{
+    value: Sig.Signal<string>,
     options: string[],
     key: string
-}> = ({value, options, key, ...props}) => {
+}> = ({ value, options, key, ...props }) => {
     return (
-        
+
         <div>
-            <datalist id={"list_"+key}> 
-                { options.map((option) => <option value={option}/>) }
+            <datalist id={"list_" + key}>
+                {options.map((option) => <option value={option} />)}
             </datalist>
-            <Input 
-                value={value} 
-                onChange={(e) => value.setValue(() => e.target.value) }
-                list={"list_"+key} 
-                { ...props }
+            <Input
+                value={value}
+                onChange={(e) => value.setValue(() => e.target.value)}
+                list={"list_" + key}
+                {...props}
             />
         </div>
-            
-
     );
 }
 
+
+
+export const Checkbox: Sig.FC<{
+    value: Sig.Signal<boolean>,
+    label: string,
+    id: string
+}> = ({ value, label, id, ...props }) => {
+    return (
+        <div style={{ display: 'flex'}}>
+            <input
+                type="checkbox"
+                id={id}
+                checked={value as unknown as boolean}
+                onChange={(e) => value.setValue(() => e.target.checked)}
+                {...props}
+            />
+            <label htmlFor={id}>{label}</label>
+        </div>
+    );
+}
--- packages/examples/src/app/example-form/index.tsx
+++ packages/examples/src/app/example-form/index.tsx
@@ -1,11 +1,9 @@
 
-import { Show, signal, signalComponent, derivedSignal, FC } from 'sig';
+import { signal, signalComponent, FC } from 'sig';
 import './form-example.scss';
-import { Dropdown, Input } from './controllers'
-type FormErrors = {
-  firstName?: string;
-  lastName?: string;
-};
+import { Checkbox, Dropdown, Input, InputController } from './controllers'
+
+
 type FormValues = {
   firstName: string;
   lastName: string;
@@ -18,84 +16,50 @@ const initialFormValues: FormValues = {
   color: 'red'
 
 };
-const validateForm = (values: FormValues): FormErrors => {
-  const errors: FormErrors = {};
 
-  if (!values.firstName) {
-    errors.firstName = 'First name is required';
-  }
-
-  if (!values.lastName) {
-    errors.lastName = 'Last name is required';
-  }
-
-  return errors;
-};
 
 
 const Form: FC = function Form() {
   const formFirstNameValues = signal(initialFormValues.firstName);
   const formLastNameValues = signal(initialFormValues.lastName);
   const formColorValues = signal(initialFormValues.color);
-  const formErrors = signal<FormErrors>({});
+  const formNoValidate = signal(false);
 
   const handleSubmit = (event: Event) => {
     event.preventDefault();
-    const errors = validateForm({
-      firstName: formFirstNameValues.value,
-      lastName: formLastNameValues.value
-    });
-    formErrors.setValue(() => errors);
-    if (Object.keys(errors).length === 0) {
-      alert(
-        `${formFirstNameValues.value}, ${formLastNameValues.value}`,
-      );
-    }
+    // alert("Form submitted");
   };
 
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="firstName">First Name:</label>
-        <Input
-          type="text"
-          id="firstName"
-          value={formFirstNameValues}
-          onInput={(e) =>
-            formFirstNameValues.setValue(() => (e.target as HTMLInputElement).value)
-          }
-        />
-        <Show
-          track={formErrors}
-          when={([errors]) => !!(errors.firstName)}
-        >
-          <label className="error">{derivedSignal(formErrors, (f => f.firstName))}</label>
-        </Show>
-      </div>
+    <form 
+      // noValidate={formNoValidate as unknown as boolean}
+      onSubmit={handleSubmit}>
 
-      <div>
-        <label htmlFor="lastName">Last Name:</label>
-        <Input
-          type="text"
-          id="lastName"
-          value={formLastNameValues}
-          onInput={(e) =>
-            formLastNameValues.setValue(() => (e.target as HTMLInputElement).value)
-          }
-        />
-        <Show
-          track={formErrors}
-          when={([errors]) => !!(errors.lastName)}
-        >
-          <label className="error">{derivedSignal(formErrors, (f => f.lastName))}</label>
-        </Show>
-      </div>
-
-      <Dropdown
+      <InputController 
+        value={formFirstNameValues}
+        label="First Name"
+        id="firstName"
+        required
+        validation={(value) => !!value}
+        validationError="First name is required"
+      />
+      <InputController  
+        value={formLastNameValues}
+        label="Last Name"
+        id='Last Name'
+        required
+        validation={(value) => !!value}
+        validationError="Last name is required"
+       />
+      {/* <Dropdown
         value={formColorValues}
         options={['red', 'blue']} 
         key="color"
+      /> */}
+      <Checkbox 
+        value={formNoValidate}
+        label="No Validate"
+        id="no-validate"
       />
       <button type="submit">Submit</button>
     </form>
