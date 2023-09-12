16c3b1a89f422f5dab442529c0b356caabbff536
og8217 <og8217@intl.att.com>
2023-09-12 15:03:16 +0300

    update build config

--- packages/sig/vite.config.ts
+++ packages/sig/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import path from 'path';
+import pkg from './package.json';
 
 export default defineConfig({
     build: {
         lib: {
           entry: path.resolve(__dirname, "lib/index.ts"),
-          name: "sig",
+          name: pkg.name,
           fileName: "index",
         },
       },
