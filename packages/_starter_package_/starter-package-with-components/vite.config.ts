89330e6c6d5d9c9fdce3bfc433e3673dae791b13
og8217 <og8217@intl.att.com>
2023-09-25 15:18:35 +0300

    reorder starter packages

---
+++ packages/_starter_package_/starter-package-with-components/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import dts from 'vite-plugin-dts';
+import pkg from './package.json';
+
+export default defineConfig({
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, "lib/index.ts"),
+            name: pkg.name,
+            fileName: "index",
+        },
+        rollupOptions: {
+            external: ['sig'],
+            output: {
+                globals: {
+                    sig: 'Sig',
+                },
+            },
+        },
+    },
+    esbuild: {
+        jsxFactory: '__createElement',
+        jsxFragment: '__createFragment',
+        jsxInject: `import { createFragment as __createFragment, createElement as __createElement } from 'sig'`,
+    },
+    css: { modules: { localsConvention: 'camelCase' } },
+    plugins: [dts()],
+});
