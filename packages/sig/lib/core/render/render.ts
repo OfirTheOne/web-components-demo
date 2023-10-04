6a2d8b9dc7440b285444737cc0daef330fa33549
og8217 <og8217@intl.att.com>
2023-10-05 00:26:45 +0300

    render only onload

--- packages/sig/lib/core/render/render.ts
+++ packages/sig/lib/core/render/render.ts
@@ -4,9 +4,19 @@ import { signalRender } from './signal-render';
 import { DOM } from '@sig/dom';
 
 export function render(elem: JSX.Element | VirtualElement, id: string) {
+  if (window.document.readyState === 'complete') {
+    renderRoot(elem, id);
+  } else {
+    window.onload = () => {
+      renderRoot(elem, id);
+    };
+  }
+}
+
+function renderRoot(elem: JSX.Element | VirtualElement, id: string) {
   const vElem = elem as VirtualElement;
   const element = signalRender(null, vElem, ComponentKey.build().root().toString());
   Array.isArray(element)
-    ? element.forEach((node) =>  DOM.treeManipulation.appendToParent(DOM.access.getElementById(id), node))
+    ? element.forEach((node) => DOM.treeManipulation.appendToParent(DOM.access.getElementById(id), node))
     : DOM.treeManipulation.appendToParent(DOM.access.getElementById(id), element);
 }
