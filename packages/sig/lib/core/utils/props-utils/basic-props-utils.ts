383330bc96d6dd4c3edfd12aba0c5c8729b66792
og8217 <og8217@intl.att.com>
2023-10-06 15:50:26 +0300

    remove unused function

--- packages/sig/lib/core/utils/props-utils/basic-props-utils.ts
+++ packages/sig/lib/core/utils/props-utils/basic-props-utils.ts
@@ -9,7 +9,6 @@ export class BasicPropsUtils {
             classNameDict: {} as Record<string, boolean>,
             toggleClassList: {} as Record<string, Trackable<unknown>>,
             replaceClassList: [] as Trackable<unknown>[],
-    
         }
         flattenClassList.forEach(
             (item) => {
@@ -29,21 +28,6 @@ export class BasicPropsUtils {
                 }
             });
         return classDict;
-    }
-
-    static mutateBasicProps(props: Record<string, unknown>) {
-        if (props['class:list']) {
-            if (props['className']) {
-                console.warn(`class:list and className are mutually exclusive. className will be ignored`);
-                delete props['className'];
-            }
-            const resolvedClassList = props['class:list'];
-            props['class:list'] = resolvedClassList;
-        } else if (props['className']) {
-            props['class'] = props['className'];
-            delete props['className'];
-        }
-        return props;
     }
 }
 
