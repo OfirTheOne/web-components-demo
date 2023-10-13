eaff5de06976535ea880dc2c66c12507291f5aa7
og8217 <og8217@intl.att.com>
2023-10-13 03:40:11 +0300

    implement dispose for DecoratedSignal

--- packages/signal/lib/decorated-signal.ts
+++ packages/signal/lib/decorated-signal.ts
@@ -1,30 +1,36 @@
-import { ISignal } from "./types/i-signal";
+import { ISignal, ISubscribableSignal } from "./types/i-signal";
 import { IDecoratedSignal } from "./types/i-decorated-signal";
 
-export class DecoratedSignal<N=unknown> implements IDecoratedSignal<N> {
-    readonly source: ISignal<unknown>;
-    protected wrappedListenersToOriginal: WeakMap<((value: N) => void), (value: unknown) => void> = new WeakMap();
+export class DecoratedSignal<N=unknown, S extends ISignal<N> | ISubscribableSignal<N> = ISignal<N>> implements IDecoratedSignal<N, S> {
+    get value(): N {
+        return this.computeValue();
+    }
+    get id(): string {
+        return this.source.id;
+    }
+    readonly source: S;
+    protected wrappedListenersToOriginal: Map<((value: N) => void), (value: unknown) => void> = new Map();
 
-    constructor(source: ISignal<unknown>) {
+    constructor(source: S) {
         this.source = source;
     }
-
-    subscribe(listener: (value: N) => void) {
+    subscribe(listener: (value: N) => void): () => void {
         const wrappedListener = () => listener(this.computeValue())
         this.wrappedListenersToOriginal.set(listener, wrappedListener);
         this.source.subscribe(wrappedListener);
+        return () => this.unsubscribe(wrappedListener);
+    }
+    dispose(): void {
+        this.wrappedListenersToOriginal.forEach((wrappedListener, originalListener) => {    
+            this.source.unsubscribe(wrappedListener);
+            this.wrappedListenersToOriginal.delete(originalListener);
+        });
     }
     unsubscribe(listener: (value: N) => void): void {
         const wrappedListener = this.wrappedListenersToOriginal.get(listener);
         if (wrappedListener) {
             this.source.unsubscribe(wrappedListener);
         }
-    }
-    get value(): N {
-        return this.computeValue();
-    }
-    get id(): string {
-        return this.source.id;
     }
     computeValue(): N {
         return this.source.value as N;
